b883b55525506e7814a0ea07ca27c642c21fb938
Eric D. Torres <etorresnotary@gmail.com>
2020-09-06 10:56:58 -0500

    loading function

--- app.js
+++ app.js
@@ -4,39 +4,61 @@ const postContainerEl = document.querySelector("#post-container");
 const loadingEl = document.querySelector(".loader");
 const filterEl = document.querySelector("#filter");
 
-let limit = 3;
+let limit = 5;
 let page = 1;
-
 
 //fetch posts from API
 async function getPosts() {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`)
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
+  );
 
-    const data = await res.json();
+  const data = await res.json();
 
-    return data;
+  return data;
 }
 
 //shohw posts in DOM
 async function showPosts() {
-    const posts = await getPosts();
-    // console.log(posts);
+  const posts = await getPosts();
+  // console.log(posts);
 
-    posts.forEach(post => {
-        const postEl = document.createElement("div");
-        postEl. classList.add("post");
-        postEl.innerHTML = `
+  posts.forEach((post) => {
+    const postEl = document.createElement("div");
+    postEl.classList.add("post");
+    postEl.innerHTML = `
         <div class="number">${post.id}</div>
         <div class="post-info">
         <h2 class="post-title">${post.title}</h2>
         <p class="post-body">${post.body}</p>
         </div>
-        `
+        `;
 
-        postContainerEl.appendChild(postEl);
-    });
+    postContainerEl.appendChild(postEl);
+  });
+}
 
+//show loader and fetch more posts
+function showLoading (){
+    loadingEl.classList.add("show");
+
+
+    setTimeout(() => {loadingEl.classList.remove("show")}, 1500);
+
+    setTimeout(()=> {
+        page++;
+        showPosts();
+    }, 2000)
 }
 
 //show initial posts
 showPosts();
+
+//event listener on window
+window.addEventListener("scroll", () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    showLoading();
+  }
+});
